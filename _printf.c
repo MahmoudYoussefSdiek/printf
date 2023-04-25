074c348c0e1bd728e5a72f1de86c07e827c295f3
jihadsheble <jihadsheble@gmail.com>
2023-04-25 14:42:06 -0700

    Update _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,53 +1,6 @@
 #include "main.h"
 
 /**
- * int_to_string - function that take int and save it in array as string.
- *
- * @number: number that we will save it as string.
- * @buffer: array that we will save the string.
- *
- * Return: void.
- */
-void int_to_string(int number, char *buffer)
-{
-	sprintf(buffer, "%d", number);
-}
-
-/**
- * print_char - function that prints a character to standard output.
- *
- * @character: the character that we will print.
- *
- * Return: 1 on success or -1 on error
- */
-int print_char(char character)
-{
-	return (write(1, &character, 1));
-}
-
-/**
- * print_string - prints a string to standard output.
- *
- * @string: the string that we will print.
- *
- * Return: number of characters printed (string length)
- */
-int print_string(char *string)
-{
-	int index = 0;
-
-	if (string == NULL)
-		string = "(null)";
-
-	while (string[index] != '\0')
-	{
-		print_char(string[index]);
-		index++;
-	}
-
-	return (index);
-}
-/**
  * _printf -  function that produces output according to a format.
  *
  * @format: the format string that we will print.
