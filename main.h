ca1b33921d38c94beb6ea6b676fd4f8a34c2c262
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-25 18:30:35 -0700

    Update main.h file

--- main.h
+++ main.h
@@ -7,6 +7,19 @@
 
 #define BUFF_SIZE 1024
 
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct pair_function_symbol
+{
+	char symbol;
+	int (*function)(va_list);
+};
+typedef struct pair_function_symbol call_function;
+
 int _printf(const char *format, ...);
 int print_char(char character);
 int print_string(char *string);
